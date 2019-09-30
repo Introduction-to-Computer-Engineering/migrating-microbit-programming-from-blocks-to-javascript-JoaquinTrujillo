8479fb1ede1391dfc3a00def8dc209f2e538c118
JoaquinTrujillo <54654420+JoaquinTrujillo@users.noreply.github.com>
2019-09-30 00:01:00 -0600

    Add files via upload

--- module-info.java
+++ module-info.java
@@ -1,64 +1,21 @@
-module microbit8 {input.onGesture(Gesture.Shake, function () {
-    coinHeadA2 = Math.randomBoolean()
-    coinHeadB2 = Math.randomBoolean()
-    if (coinHeadA2 == coinHeadB2) {
-        basic.showLeds(`
-            . . # . .
-            . # . # .
-            . # # # .
-            . # . # .
-            . # . # .
-            `)
-        basic.pause(1000)
-        playerOneScore += 1
-    } else {
-        basic.showLeds(`
-            # # # # .
-            # . . . #
-            # # # # #
-            # . . . #
-            # # # # .
-            `)
-        basic.pause(1000)
-        playerTwoScore += 1
-    }
-    basic.showLeds(`
-        . # . . .
-        # # # . .
-        . # . # .
-        . . # # #
-        . . . # .
-        `)
+module microbit9 {let binary = ""
+let decimal = 0
+input.onButtonPressed(Button.A, () => {
+    binary = binary + "1"
+    basic.showString("BINARY+1")
+    decimal = decimal * 2 + 1
 })
-input.onButtonPressed(Button.A, function () {
-    coinHeadB3 = true
-    coinheadA = true
-    basic.showString("A")
-    basic.showNumber(playerOneScore)
-    basic.showString("B")
-    basic.showNumber(playerTwoScore)
-    basic.showLeds(`
-        . # . . .
-        # # # . .
-        . # . # .
-        . . # # #
-        . . . # .
-        `)
+input.onButtonPressed(Button.AB, () => {
+    basic.showNumber(decimal)
 })
-let coinHeadB3 = false
-let playerTwoScore = 0
-let playerOneScore = 0
-let coinHeadB2 = false
-let coinHeadA2 = false
-let coinheadA = false
-coinheadA = false
-let coinHeadB = false
-basic.showLeds(`
-    . # . . .
-    # # # . .
-    . # . # .
-    . . # # #
-    . . . # .
-    `)
-
+input.onButtonPressed(Button.B, () => {
+    binary = binary + "0"
+    basic.showString("BINARY+0")
+    decimal = decimal * 2
+})
+input.onGesture(Gesture.Shake, () => {
+    binary = "0"
+    decimal = 0
+    basic.showString("CLEAR")
+})
 }
